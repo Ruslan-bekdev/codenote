993d420b4369b2e9aa8608260d95cd7e9720eae1
Ruslanbek <toopikoliko2@gmail.com>
2025-03-22 01:30:34 +0600

    удаление временной иконки для теста и пополнение текста

--- src/dataBase/errors.js
+++ src/dataBase/errors.js
@@ -102,5 +102,46 @@ export default {
               'Указать почту редактору ГЛОБАЛЬНО чтобы на другие проекты тоже распространялось\n' +
               'git config --global user.email "toopikoliko2@gmail.com"\n',
         },
+        {
+          summary: 'Ошибка при деплое vite проекта ещё одна',
+          content: 'Fatal process out of memory: Zone Allocation failed\n' +
+              '<--- Last few GCs --->\n' +
+              '[3792:00000142F4241000]     2340 ms: Scavenge 91.2 (107.8) -> 77.5 (108.8) MB, pooled: 0 MB, 3.45 / 0.00 ms  (average mu = 0.973, current mu = 0.973) allocation failure;\n' +
+              '[3792:00000142F4241000]     2404 ms: Scavenge 92.4 (109.1) -> 78.4 (110.6) MB, pooled: 0 MB, 2.64 / 0.00 ms  (average mu = 0.973, current mu = 0.973) allocation failure;\n' +
+              '\n' +
+              '<--- JS stacktrace --->\n' +
+              'FATAL ERROR: Zone Allocation failed - process out of memory\n' +
+              '----- Native stack trace -----\n' +
+              ' 1: 00007FF7A30B8F3D\n' +
+              ' 1: 00007FF7A30B8F3D\n' +
+              ' 2: 00007FF7A2F9956F\n' +
+              ' 3: 00007FF7A4357126\n' +
+              ' 2: 00007FF7A2F9956F\n' +
+              ' 3: 00007FF7A4357126\n' +
+              ' 4: 00007FF7A3D06631\n' +
+              ' 5: 00007FF7A37AC5DB\n' +
+              ' 6: 00007FF7A369A143\n' +
+              ' 7: 00007FF7A36A1849\n' +
+              ' 8: 00007FF7A367193A\n' +
+              ' 9: 00007FF7A36A1181\n' +
+              '10: 00007FF7A36A9B96\n' +
+              '11: 00007FF7A36B3CBB\n' +
+              '12: 00007FF7A3685E5F\n' +
+              '13: 00007FF7A36D2A18\n' +
+              '14: 00007FF7A36D2E64\n' +
+              '15: 00007FF7A36B7901\n' +
+              '16: 00007FF7A3C82124\n' +
+              '17: 00007FF7A36B8E42\n' +
+              '18: 00007FF7A3F11E15\n' +
+              '19: 00007FF7A2F9BE09\n' +
+              '20: 00007FF7A311AA43\n' +
+              '21: 00007FF7A4A95A9E\n' +
+              '22: 00007FF802A77034 BaseThreadInitThunk+20\n' +
+              '23: 00007FF802F62651\n' +
+              '\n' +
+              'РЕШЕНИЕ' +
+              'Ошибка указывает на то, что данных слишком много. Нужно просто очистить кэш \n' +
+              'npm cache clean --force\n',
+        },
     ]
 };
